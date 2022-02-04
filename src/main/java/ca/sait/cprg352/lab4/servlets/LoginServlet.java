51b1fe4fa658b8a4eb433e0e241f8bcb16792c1c
Lisa Jowett <lisajowett@live.ca>
2022-02-04 14:36:34 -0700

    Initial servlet and jsp setup

---
+++ src/main/java/ca/sait/cprg352/lab4/servlets/LoginServlet.java
@@ -0,0 +1,56 @@
+package ca.sait.cprg352.lab4.servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Lisa Jowett
+ */
+public class LoginServlet extends HttpServlet 
+{
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String query = request.getQueryString();
+
+        if(query != null && query.contains("logout"))
+        {
+
+        }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String userName = request.getParamter("userName");
+        String password = request.getParamter("password");
+
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+
+}
